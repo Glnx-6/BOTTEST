f9b5ee3e0493bc942a0694389fac71b48470876a
Glnx-6 <74367098+Glnx-6@users.noreply.github.com>
2024-06-19 22:35:08 +0700

    A

---
+++ config.js
@@ -0,0 +1,132 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const axios = require('axios');
+
+// INGAT CUY INI CUMA SC HASIL RENAME ATAU RECODE YA
+
+// Credit : @theJayy_
+// Ori : @callmevinz
+
+//=== Edit Disini 🔥
+global.namabot = "Rhin"
+global.namaowner = "LANG"
+global.packname = "Sticker by"
+global.creator = "Rhin"
+global.author = "AiBotzz-MD\n\nBot WhatsApp"
+global.wm = "RhinBOT"
+global.syt = "https://www.youtube.com/@SanjayaAds"
+global.sgc = "-"
+global.idgc = "-"
+global.email = "rahasia@gmail.com"
+global.sig = "-"
+global.myweb = "https://glamour.sijaa26.site"
+global.footer_text = "© RhinBOT"
+global.owner = ['6285652318924']
+global.gifin = "https://v1.pinimg.com/videos/iht/720p/a3/8b/ae/a38baec375918a9a4a54f921af57d04b.mp4"
+global.thumb = "https://i.pinimg.com/736x/ca/ac/f7/caacf78c8e65cf8879ebbae0d6229f06.jpg"
+global.thumb2 = "https://i.pinimg.com/736x/9d/02/9a/9d029a03c36e5af1d6184eb23cbe358a.jpg"
+global.mark = "https://telegra.ph/file/9b67cb5ed0ff661027366.jpg"
+global.themeemoji = '🌐'
+
+//=== Introgasi 🗿
+global.umurowner = "Privat" // Terserah🗿
+global.kelasowner = "XI PERTANIAN" // Terserah🗿
+global.statusowner = "<3 Zeya" // Terserah
+global.lakiapacewek = "Siap GYATT" // Terserah
+// Kalo mau gak diisi juga gak papa
+
+//=== Payment 😋
+global.qris = "-"
+global.pulsa = "-"
+global.dana = "-"
+global.gopay = "-"
+global.rek = "-"
+
+//=== Nokos Token 😱
+global.apikey ='8c3a5b302815a138d88148fa0c5916c0595bba50' 
+
+//=== Apikey Nya 🔥
+global.lol = 'GataDios'
+global.lol = 'SGWN'
+global.rose = 'Rs-putangina'
+global.xyro = '5dRkJDWvIG'
+global.APIs = {
+xyro: "https://api.xyroinee.xyz",
+popcat : 'https://api.popcat.xyz'
+}
+// APIKeys
+global.APIKeys = {
+"https://api.xyroinee.xyz": "5dRkJDWvIG"
+}
+
+//=== Gak Tau 🐦
+global.pairingNumber = "6285225148933"
+// Nomor whatsapp bot lu
+global.prefix = ['-_-'] 
+// Jangan diubah
+global.tekspushcontact = "Izin Push" 
+// Terserah
+global.typemenu = "v6"
+// Ini type menu nya
+// v1 - v2 - v3 - v4 - v5 - v6
+global.typeallmenu = "v1"
+// Ini type allmenu nya
+// v1 - v2 - v3 - v4 - v5 - v6
+global.game = true
+// False (Nonaktifin)
+global.groupOnly = false 
+// True (Mode grup)
+global.privateOnly = false
+// True (Mode grup)
+global.antispam = true 
+// False (Nonaktifin)
+global.anticall = true
+// False (Nonaktifin)
+global.autoreadsw = true
+// False (Nonaktifin)
+global.antiBot = true
+// False (Nonaktifin)
+global.pengingat = true
+// False (Nonaktifin)
+global.autoTyping = true
+// False (Nonaktifin)
+global.autoBio = true
+// False (Nonaktifin)
+global.autoRestart = true
+// False (Nonaktifin)
+// AutoRestart Cocok Tuk Panel Butut
+global.mess = {
+ done: 'Sukses!',
+ wait: 'Sedang diproses',
+ admin: 'Fitur Khusus Admin grup!',
+ botNotAdmin: 'Saya bukan Admin grup',
+ owner: 'Fitur Khusus Owner!',
+ group: 'Fitur Khusus Group!',
+}
+
+// Setting Game
+global.gamewaktu = 60 // Game waktu
+global.bmin = 1000 // Balance minimal 
+global.bmax = 5000 // Balance maksimal
+global.limit = 30 // Limit user
+
+// Database Game
+global.suit = {};
+global.tictactoe = {};
+global.petakbom = {};
+global.kuis = {};
+global.siapakahaku = {};
+global.asahotak = {};
+global.susunkata = {};
+global.caklontong = {};
+global.family100 = {};
+global.tebaklirik = {};
+global.tebaklagu = {};
+global.tebakgambar = {};
+global.tebakkimia = {};
+global.tebakkata = {};
+global.tebakkalimat = {};
+global.tebakbendera = {};
+global.tebakanime = {};
+global.kuismath = {};
+
